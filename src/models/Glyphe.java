4307e258603e4610e325ee132f86cfe672e47c45
PrPopCorn <nicolas.palpacuer@live.fr>
2015-11-14 20:02:13 -0500

    Initial files
    Skeleton of the models

---
+++ src/models/Glyphe.java
@@ -0,0 +1,7 @@
+package models;
+
+/**
+ * Created by Nicolas on 11/14/2015.
+ */
+public abstract class Glyphe extends Rune {
+}
---
+++ src/models/RunePage.java
@@ -0,0 +1,22 @@
+package models;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class RunePage {
+
+	private List<Rune> runes;
+	
+	public RunePage(){
+		runes = new LinkedList<Rune>();
+	}
+
+	public boolean addRune(Rune newRune){
+		//Check if allowed
+
+		return  true;
+	}
+
+	
+}
